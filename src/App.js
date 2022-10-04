756f058cfbf4ed01169e40d9a554f7c5fbdc3105
MercyChebet <chebetm96@gmail.com>
2022-10-04 21:43:36 +0300

    CReating landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from './Components/NavBar';
 import './App.css';
+import Home from './Components/Home'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+function Home () {
+    return (
+        <div className="landingpage">
+            <h1>
+                Become Fitter, Stronger, & More Confident
+            </h1>
+            
+        </div>
+    )
+}
+export default Home;
