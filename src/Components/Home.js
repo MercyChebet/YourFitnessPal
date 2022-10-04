dd8689bf572cfb87a7795f8f45d714debb5820c5
MercyChebet <chebetm96@gmail.com>
2022-10-04 22:03:48 +0300

    editing css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,17 @@
+function HowItWorks () {
+    return (
+        <div className="Howitworks">
+            <p>Build your body and fitness with a professional touch</p>
+        </div>
+    )
+}
 function Home () {
     return (
         <div className="landingpage">
             <h1>
                 Become Fitter, Stronger, & More Confident
             </h1>
-            
+         <HowItWorks />
         </div>
     )
 }
